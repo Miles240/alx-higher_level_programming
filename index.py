2d4a189e75b5c141328489d0c0af3f5a6ee93709
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-28 20:59:50 +0100

    created empty files

---
+++ index.py
@@ -0,0 +1,23 @@
+# investment = input("Amout: ")
+# interest = input("Interest: ")
+
+# investment = float(investment)
+# interest = float(interest) * 0.01
+
+
+# for i in range(10):
+#     investment = investment + (investment * interest)
+
+# print("Expect : {:.2f} after 10 years".format(investment))
+
+
+# a = 12
+# if a < 2:
+#     print("Holberton")
+# elif a % 2 == 0:
+#     print("C is fun")
+# else:
+#     print("School")
+
+for i in range(4):
+    print(i, end=" ")
