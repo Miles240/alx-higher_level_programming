a64a64aa1bd672e9a4ba0e679c8630dad0a570d0
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-29 16:01:21 +0100

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+for i in range(0, 10):
+    for n in range(1, 10):
+        if n > i:
+            print("{}{}".format(i, n), end="")
+            if i != 8 or n != 9:
+                print(", ", end="")
+print("")
