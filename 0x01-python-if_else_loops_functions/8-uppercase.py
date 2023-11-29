7bd9c5616d1d801a803b0b6a754e8bdb350f0b17
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-29 16:15:37 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if "a" <= char <= "z":
+            uppercase_char = chr(ord(char) - ord("a") + ord("A"))
+            result += uppercase_char
+        else:
+            result += char
+    return result
