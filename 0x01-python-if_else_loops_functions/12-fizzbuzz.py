40455fa60d84b64e0fc1ae24feaabb2a90dea350
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-29 02:32:33 +0100

    fizzy

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print(f"Fizz Buzz", end=" ")
+            print(f"FizzBuzz", end=" ")
             continue
         elif i % 3 == 0:
             print(f"Fizz", end=" ")
