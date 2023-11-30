01ec2c90d80698ea2449e7d24b52a1225dbe9f4c
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-30 23:39:58 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+num_args = len(sys.argv) - 1
+
+if num_args == 0:
+    print("{} arguments.".format(0))
+else:
+    print("{} arguments:".format(num_args))
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, sys.argv[i]))
