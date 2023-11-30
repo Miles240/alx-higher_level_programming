24e78c5d1369b3435a5086be418ab1e315d299ef
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-30 22:20:40 +0100

    add.com

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ from add_0 import add
 a = 1
 b = 2
 
-print(int("{} + {} = {}").format(a, b,( add(a, b))))
+print(int("{} + {} = {}".format(a, b, add(a, b))))
 
 if __name__ == "__main__":
     add(a, b)
