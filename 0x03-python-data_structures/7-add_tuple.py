041752c03b510b9ebbee75fd6bf6ed552f01eb44
Miles240 <piuschukwuebuka40@gmail.com>
2023-12-04 23:25:21 +0100

    tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,10 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    result = tuple(x + y for x, y in zip(tuple_a, tuple_b))
-    return result
+    # Ensure each tuple has at least 2 elements
+    tuple_a = tuple_a[:2] + (0, 0)
+    tuple_b = tuple_b[:2] + (0, 0)
+
+    # Perform element-wise addition and return a new tuple
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result_tuple
