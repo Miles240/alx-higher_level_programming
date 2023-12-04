0625b604c1daa06485ac6a29844caeabd3d7942a
Miles240 <piuschukwuebuka40@gmail.com>
2023-12-04 15:10:40 +0100

    push mf

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,3 +5,7 @@ if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for num in my_list:
             print("{}".format(num))
+
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
