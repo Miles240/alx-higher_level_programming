1a3c49f91ce32ca0097f6898637a0d52c7993eee
Miles240 <piuschukwuebuka40@gmail.com>
2023-12-04 14:48:48 +0100

    ,.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     list_len = len(my_list)
     for i in range(0, list_len):
         print("{}".format(my_list[i]))
+
 
 if __name__ == "__main__":
     print_list_integer()
