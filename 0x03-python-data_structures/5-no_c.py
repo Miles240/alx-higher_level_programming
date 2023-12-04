e639e22728597763ed6def3f38943e5e43fb25f1
Miles240 <piuschukwuebuka40@gmail.com>
2023-12-04 19:02:08 +0100

    push

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char == "c" or char == "C":
+            continue
+        result += char
+    return result
