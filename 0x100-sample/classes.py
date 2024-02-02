d5c2c22bf2d573692a4a41d0cc9756cb96e7e393
Miles <piuschukwuebuka40@gmail.com>
2024-02-02 06:43:00 +0100

    ...

--- 0x100-sample/classes.py
+++ 0x100-sample/classes.py
@@ -1,34 +1,59 @@
-# class Square:
-#     def __init__(self, size):
-#         self._size = size
+class Rectangle:
+    """Creating a Triangle
+    """
+    def __init__(self, width=0, height=0):
+        """Intializing the triangle"""
+        self.width = width
+        self.height  = height
 
-#     @property
-#     def size(self):
-#         return self._size
+    def __del__(self):
+        print("Bye rectangle...")
 
-#     @size.setter
-#     def size(self, value):
-#         self._value = value
-#         if not int(self._value):
-#             raise TypeError("size must be an integer")
-#         elif self._value < 0:
-#             raise ValueError("size must be >= 0")
+    @property
+    def width(self):
+        """Getter for private attribute width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Setter for private attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-#     def __init(self, size=0):
-#         self.size = size
+    @property
+    def height(self):
+        """getter for private attribute height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """setter for private attribute heigth"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-#     def area(self):
-#         return self._size**2
-
-
-class Student:
-    def __init__(self, name, grade):
-        self._name = name
-        self._grade = grade
-
-    def __repr__(self):
-        return f"{self._name}, {self._grade}"
-
-mary = Student("Mary", 40)
-mary_str  = repr(mary)
-print(mary_str)
+    def area(self):
+        """Area of a rectangle"""
+        return (self.__width * self.__height)
+    
+    def perimeter(self):
+        """Perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width *2) + (self.__height * 2)
+    
+    def __str__(self):
+        string = ""
+        string += '\n'.join('#' * self.__width for i in range(self.__height))
+        return string
+    
+    def __repr__(self) :
+        return f"Rectangle ({self.__width}, {self.__height})"
+    
