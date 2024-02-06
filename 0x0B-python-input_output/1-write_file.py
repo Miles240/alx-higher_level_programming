d5f2d60b5d7384eb8ceccb712779ec3082b9d4ed
Miles <piuschukwuebuka40@gmail.com>
2024-02-06 23:15:49 +0100

    writing in a new file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains a write_file function"""
+
+
+def write_file(filename="", text=""):
+    """Creates/Write in a file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(str(text))
