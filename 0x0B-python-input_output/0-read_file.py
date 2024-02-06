a395c0dcfd273a09529f0e79df73259f05dcf08c
Miles <piuschukwuebuka40@gmail.com>
2024-02-06 21:15:59 +0100

    himput

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 def read_file(filename=""):
     """Reads a text file and prints it to Stdout"""
 
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         for line in file:
-            print(line, end='')
+            print(line, end="")
