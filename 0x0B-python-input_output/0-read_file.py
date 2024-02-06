801443455493b548cda7dfa8f4cb313f01240cfe
Miles <piuschukwuebuka40@gmail.com>
2024-02-06 21:07:27 +0100

    output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """Reads a text file and prints it to Stdout"""
 
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        file.read()
