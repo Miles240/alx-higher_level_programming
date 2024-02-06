89bad5c3887e3f9efcee3223d4ace90a3069f0c6
Miles <piuschukwuebuka40@gmail.com>
2024-02-06 18:58:49 +0100

    py

--- 0x100-sample/phonebook.py
+++ 0x100-sample/phonebook.py
@@ -1,16 +1,5 @@
 import csv
 
-# file = open("phonebook.csv", "a")
-
-# name = input("Name: ")
-# number = input("Number: ")
-
-# writer = csv.writer(file)
-
-# writer.writerow([name,  number])
-# file.close()
-
-
 with open("Grade book - Overview.csv", "r") as file:
     name = input("name: ")
     writer = csv.writer(file)
