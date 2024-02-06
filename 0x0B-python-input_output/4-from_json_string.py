6defe453a303787c3cdb82232b6ea6f42817068a
Miles <piuschukwuebuka40@gmail.com>
2024-02-07 00:32:27 +0100

    json to dict

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""Contains a from_json_string function"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Converts a json string to a python dict"""
+    return json.loads(my_str)
