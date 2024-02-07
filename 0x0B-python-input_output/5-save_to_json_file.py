81b34de4962bf1962512c13df45dec96270b6732
Miles <piuschukwuebuka40@gmail.com>
2024-02-07 01:29:54 +0100

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Saves an object to a json string to a new file"""
-    with open(filename, "w") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,6 +8,5 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
 
-    with open(filename, "r") as file:
-        r_file = file.read()
-        json.loads(r_file)
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.loads(file)
