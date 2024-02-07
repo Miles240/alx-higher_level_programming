afef7ef5b220419aa1280260054a63e75fc2d21b
Miles <piuschukwuebuka40@gmail.com>
2024-02-07 11:25:25 +0100

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Adds all argument to a new json file"""
+import os.path
 from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
@@ -7,11 +8,11 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
+if not os.path.isfile(filename):
+    json_data = []
+else:
+    json_data = load_from_json_file(filename)
 
-for agr in argv[1:]:
-    json_list.append(agr)
-save_to_json_file(json_list, filename)
+for arg in argv[1:]:
+    json_data.append(arg)
+save_to_json_file(json_data, filename)
