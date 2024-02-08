6aa7e7647c37b5c340153b599cb6172b4d77115f
Miles <piuschukwuebuka40@gmail.com>
2024-02-08 14:07:53 +0100

    python

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student object with first_name, last_name, and age.
+        Args:
+            first_name (str): students first
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        If attrs is provided and it's a list of strings, return only the
+        attributes listed in attrs. Otherwise, return all attributes.
+        """
+        if attrs is None:
+            # If attrs is None, return all attributes in a dictionary
+            return self.__dict__
+        else:
+            # If attrs is provided and it's a list of strings,
+            # return only the attributes listed in attrs
+
+            attribute_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    attribute_dict[attr] = getattr(self, attr)
+            return attribute_dict
