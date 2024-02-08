7e19be7a11a9933937c4e2a82b8731b06fe0bba1
Miles <piuschukwuebuka40@gmail.com>
2024-02-08 14:14:29 +0100

    student info

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,16 @@
+#!/usr/bin/python3
+
+
 class Student:
+    """Defines a Student"""
+
     def __init__(self, first_name, last_name, age):
         """
         Initialize a Student object with first_name, last_name, and age.
         Args:
-            first_name (str): students first
+            first_name (str): students first name
+            last_name (str): students last name
+            age (int): students age
         """
         self.first_name = first_name
         self.last_name = last_name
