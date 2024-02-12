bb22b88b6677ce5bddf645031d32b0fe45c852f7
Miles <piuschukwuebuka40@gmail.com>
2024-02-12 09:00:21 +0100

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 
 class Base:
     """The goal of it is to manage id attribute in all your future
@@ -14,3 +15,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation"""
+        if list_dictionaries == None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        if list_objs == None:
+            with open("filename", "w") as file:
+                file.write([])
+        else:
+            with open("filename", "w") as file:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                file.write(json.dump(list_dicts))
