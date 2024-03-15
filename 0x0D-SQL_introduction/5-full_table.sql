abd5fd13e6794b264ba0833a2c70c8a5db9583fa
bg-miles <piuschukwuebuka40@gmail>
2024-03-15 08:10:36 +0100

    sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,12 @@
 -- Prints the full description of a table
-
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-AND table_name = 'first_table';
+SELECT
+    column_name,
+    data_type,
+    character_maximum_length,
+    is_nullable,
+    column_default
+FROM
+    information_schema.columns
+WHERE
+    table_schema = 'hbtn_0c_0'
+    AND table_name = 'first_table';
