bedb515560738ab6259916bc0f1626496120184b
bg-miles <piuschukwuebuka40@gmail>
2024-04-01 22:24:36 +0100

    tv shows

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,13 @@
+use hbtn_0d_tvshows;
+
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+select
+    tv_genres.name as 'name'
+from
+    tv_genres
+    join tv_show_genres on tv_show_genres.genre_id = tv_genres.id
+    join tv_shows on tv_shows.id = tv_show_genres.show_id
+where
+    tv_shows.id = 8
+order by
+    tv_genres.name desc;
