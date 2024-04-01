5838880694061c76f7da7662afa53128624c50d6
bg-miles <piuschukwuebuka40@gmail>
2024-04-01 22:27:44 +0100

    p\sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,11 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-select
-    tv_genres.name as 'name'
-from
+SELECT
+    tv_genres.name AS 'name'
+FROM
     tv_genres
-    join tv_show_genres on tv_show_genres.genre_id = tv_genres.id
-    join tv_shows on tv_shows.id = tv_show_genres.show_id
-where
+    INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE
     tv_shows.id = 8
-order by
+ORDER BY
     tv_genres.name desc;
