bc8c9151f4d5e50c3c10b986b750d1c1af9600fc
bg-miles <piuschukwuebuka40@gmail>
2024-04-01 11:23:34 +0100

    SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates a user, database and grant some privileges on the user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost';
