f12295fe987da6a2f17e5b615395af84df68782b
bg-miles <piuschukwuebuka40@gmail>
2024-04-01 13:17:00 +0100

    sasa

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
---lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT
     tv_shows.title AS 'title',
     tv_show_genres.genre_id AS 'genre_id'
