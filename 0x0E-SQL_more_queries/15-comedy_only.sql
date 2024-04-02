a269dfa6961d22aeae014dfc1798691299698a11
bg-miles <piuschukwuebuka40@gmail>
2024-04-02 14:26:37 +0100

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
 -- lists all Comedy shows in the database
-select
-    tv_shows.title as 'title'
-from
+SELECT
+    tv_shows.title AS 'title'
+FROM
     tv_shows
-    inner join tv_show_genres on tv_show_genres.show_id = tv_shows.id
-    inner join tv_genres on tv_genres.id = tv_show_genres.genre_id
-where
+    INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE
     tv_genres.id = 5
-order by
+ORDER BY
     tv_shows.title ASC
