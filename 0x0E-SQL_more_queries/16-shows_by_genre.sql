a36919ab9a5ef683fa54f58ecbf9e210fb1f64db
bg-miles <piuschukwuebuka40@gmail>
2024-04-03 13:17:32 +0100

    ...

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows, and all genres linked to that show
 SELECT
-    tv_shows.title,
-    tv_genres.name
+    tv_shows.title AS 'title',
+    tv_genres.name AS 'name'
 FROM
     tv_shows
     LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
