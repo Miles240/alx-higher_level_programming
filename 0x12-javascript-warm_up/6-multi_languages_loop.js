7cd1038725760261e6861d739f26f333809e38b9
bg-miles <piuschukwuebuka40@gmail>
2024-04-09 00:50:13 +0100

    jsx

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0; i < myArr.length; i++) {
+for (const i = 0; i < myArr.length; i++) {
   console.log(myArr[i]);
 }
