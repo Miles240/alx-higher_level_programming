69c07ea925bb51adb49051894580ae99664729a2
bg-miles <piuschukwuebuka40@gmail>
2024-04-09 00:50:56 +0100

    js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (const i = 0; i < myArr.length; i++) {
-  console.log(myArr[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	console.log(line);
 }
