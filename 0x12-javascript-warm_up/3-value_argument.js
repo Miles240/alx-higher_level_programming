7b0678afcf2e29921031462424c7ae1eb6a44a8c
bg-miles <piuschukwuebuka40@gmail>
2024-04-08 23:50:52 +0100

    js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+	console.log('No argument');
+} else if (argsCount === 1) {
+	console.log(process.argv[2]);
+} else {
+	for (let i = 2; i < process.argv.length; i++) {
+		console.log(`- ${process.argv[i]}`);
+	}
+}
