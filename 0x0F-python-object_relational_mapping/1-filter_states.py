609ce1be9a5c9625db0640cd945e636d653d3ee3
bg-miles <piuschukwuebuka40@gmail>
2024-04-12 23:19:33 +0100

    ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def list_states(username, password, name):
+    """Function that lists all the states from the database"""
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=password,
+            db=name
+        )
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        sys.exit(1)
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    list_states(username, password, name)
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def list_states(username, password, name, state_name):
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=password,
+            db=name
+        )
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        sys.exit(1)
+
+    cursor = conn.cursor()
+    query = """
+    SELECT * FROM states
+    WHERE name = '{}'
+    ORDER BY states.id ASC""".format(state_name)
+
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <name> <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    list_states(username, password, name, state_name)
