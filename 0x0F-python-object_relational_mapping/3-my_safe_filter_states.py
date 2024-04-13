dd3db26f36b16042dd0470a3be17084bb463bb78
bg-miles <piuschukwuebuka40@gmail>
2024-04-13 19:48:50 +0100

    commit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Contains the list_states Module"""
+"""Contains the list_state Module"""
 
 import MySQLdb
 import sys
@@ -8,36 +8,51 @@ import sys
 
 def list_states(username, password, name, state_name):
     """list the states in the table"""
-    conn = MySQLdb.connect(
-        host="localhost",
-        port="3306",
-        user=username,
-        password=password,
-        db=name
-    )
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=password,
+            db=name
+        )
 
-    cursor = conn.cursor()
-    query = """
-        SELECT * FROM states
-        WHERE name = %s
-        ORDER BY states.id ASC
-    """
+        cursor = conn.cursor()
 
-    cursor.execute(query, (state_name,))
-    states = cursor.fetchall()
+        query = """
+            SELECT * FROM states
+            WHERE name = %s
+            ORDER BY states.id ASC
+        """
 
-    for state in states:
-        print(state)
+        cursor.execute(query, (state_name,))
+        states = cursor.fetchall()
 
-    cursor.close()
-    conn.close()
+        for state in states:
+            print(state)
+
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to database {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(
+            """Usage: Python Script.py
+            <username>
+            <password>
+            <name>
+            <state_name>"""
+        )
+        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    state_name = sys.agrv[4]
+    state_name = sys.argv[4]
 
     list_states(username, password, name, state_name)
