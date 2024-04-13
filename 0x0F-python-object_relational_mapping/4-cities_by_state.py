56fed7b94fb419017ac02d3a6f8e14c88cfcf502
bg-miles <piuschukwuebuka40@gmail>
2024-04-13 21:28:47 +0100

    DONE

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     query = """
             SELECT cities.id, cities.name, states.name
             FROM cities
-            LEFT JOIN states ON cities.id = states.id
+            LEFT JOIN states ON cities.state_id = states.id
             ORDER BY cities.id ASC
         """
     cursor.execute(query)
