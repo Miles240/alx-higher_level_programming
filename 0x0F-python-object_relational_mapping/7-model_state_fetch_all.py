711ea9104249787cc26cd2d02e44531fb98583ea
bg-miles <piuschukwuebuka40@gmail>
2024-04-15 00:18:29 +0100

    XDF

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,10 +2,10 @@
 
 """lists all State objects from the database hbtn_0e_6_usa"""
 
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 
 if __name__ == "__main__":
@@ -25,3 +25,5 @@ if __name__ == "__main__":
 
     for state in states:
         print(state.id, state.name, sep=": ")
+
+    session.close()
