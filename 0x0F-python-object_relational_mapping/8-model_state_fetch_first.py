99a3675ff676d72c0442f3d94e77a2941cfbfd95
bg-miles <piuschukwuebuka40@gmail>
2024-04-15 00:34:07 +0100

    fetch state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" prints the first State object from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    db_host = "localhost"
+    db_port = 3306
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_URL = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    engine = create_engine(db_URL, pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).order_by(State.id.asc()).first()
+    print(result.id, result.name, sep=": ")
+
+    session.close()
