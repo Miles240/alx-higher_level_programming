73a3821fdfddc5e6feb56be3bb315f1948de3d4e
bg-miles <piuschukwuebuka40@gmail>
2024-04-15 00:37:40 +0100

    None

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).order_by(State.id).first()
+    result = session.query(State).order_by(State.id.asc()).first()
+    if result is None:
+        print("Nothing")
     print(result.id, result.name, sep=": ")
 
     session.close()
