d8780a9685a9c9c8801415752323d2d757f68b2f
bg-miles <piuschukwuebuka40@gmail>
2024-04-14 10:56:56 +0100

    NULL

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,36 +2,16 @@
 
 """contains the class definition of a State """
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
-Base = declarative_base()
+myMetadata = MetaData()
+Base = declarative_base(metadata=myMetadata)
 
 
 class State(Base):
     """Class representation of States"""
 
     __tablename__ = "states"
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-        unique=True,
-        nullable=False
-    )
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-engine = create_engine(
-    "mysql://{}:{}@localhost:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3])
-)
-
-
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
