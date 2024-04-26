e602b29cbcd8865bc08b1299ef5da8c93608fa42
bg-miles <piuschukwuebuka40@gmail.com>
2024-04-26 23:49:29 +0100

    curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-# A script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
+#!/usr/bin/bash
+# A script that takes in a URL, sends a request to that URL
 curl -s "$1" | wc -c
