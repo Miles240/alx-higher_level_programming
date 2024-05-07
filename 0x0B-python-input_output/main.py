80f3b0110a3eacc365e51b566e577a244edc42d7
bg-miles <piuschukwuebuka40@gmail.com>
2024-05-07 14:54:31 +0100

    hgf

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,13 +1,2 @@
-#!/usr/bin/python3
-Student = __import__('10-student').Student
-
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
+a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4], 'friends': [ { 'id': 82, 'name': "Bob" }, { 'id': 83, 'name': "Amy" } ] }
+print(a.get('friends')[0].get("name"))
